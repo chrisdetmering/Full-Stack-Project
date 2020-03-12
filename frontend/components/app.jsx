2a0f2aabd3d8bf5de36b2851eac4f19219b8706f
froggy <chrisdetmering@gmail.com>
2020-03-12 13:40:57 -0700

    Added ReactRouter

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-import TodoListContainer from './todos/todo_list_container'
+import TodoListContainer from './todos/todo_list_container';
+import Login from './login_logout/login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
-export const App = (props) => (<TodoListContainer />);
+export const App = (props) => (
+    
+ 
+  
+  
+  <Router> 
+    <Switch>
+      <Route path='/' component= { Login } />
+
+     
+    </Switch>
+  </Router>
+);
 
 
