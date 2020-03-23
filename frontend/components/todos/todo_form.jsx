5e524f38e557b2db283c19d3e6ac28a32084dd9f
froggy <chrisdetmering@gmail.com>
2020-03-23 16:44:12 -0700

    Added React Datetime picker to project

--- frontend/components/todos/todo_form.jsx
+++ frontend/components/todos/todo_form.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
+import Datetime from 'react-datetime';
 import { uniqueId } from '../../util/util'
 
 class TodoForm extends React.Component { 
   constructor(props) { 
     super(props)
-    this.state = { title: "", body: "", done: false }
+    this.state = { title: "", body: "", due: "", done: false }
+    this.updateProperty = this.updateProperty.bind(this);
+  }
+
+  updateProperty(e) { 
+    e.preventDefault(); 
+    var name = e.target.name; 
+    var value = e.target.value; 
+
+    this.setState({ [name]: value })
+    console.log(this.state)
   }
 
   render() {
     return (<div>
-        <p>TodoForm</p>
         <form>
-          
+          <label htmlFor="">ADD TODO</label>
+          <br/>
+
+          <input 
+            type="text" 
+            placeholder="Todo" 
+            name="title"
+            onChange={this.updateProperty}/>
+
+          <input 
+            type="text" 
+            placeholder="Note" 
+            name="body"
+            onChange={this.updateProperty}/>
+
+          <Datetime />
+
+          <button>+</button>
         </form>
       </div>)
   }
-
 }
 
 export default TodoForm; 
